d274bb23d5ab88f87fc074ab4020c5a0cdf54c4a
Wolfgang Mathurin <wmathurin@salesforce.com>
2016-03-22 17:53:17 -0700

    Android app now mostly working

--- app/android/app/src/com/salesforce/samples/myuserpicreactnative/MainActivity.java
+++ app/android/app/src/com/salesforce/samples/myuserpicreactnative/MainActivity.java
@@ -26,78 +26,45 @@
  */
 package com.salesforce.samples.myuserpicreactnative;
 
-import android.os.Bundle;
-import android.view.KeyEvent;
-
-import com.facebook.react.LifecycleState;
-import com.facebook.react.ReactInstanceManager;
-import com.facebook.react.ReactRootView;
-import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
-import com.salesforce.androidsdk.reactnative.app.SalesforceReactSDKManager;
-import com.salesforce.androidsdk.rest.RestClient;
-import com.salesforce.androidsdk.ui.SalesforceActivity;
 import com.imagepicker.ImagePickerPackage;
+import com.salesforce.androidsdk.reactnative.app.SalesforceReactSDKManager;
+import com.salesforce.androidsdk.reactnative.ui.SalesforceReactActivity;
 
-public class MainActivity extends SalesforceActivity implements DefaultHardwareBackBtnHandler {
+import java.util.Arrays;
+import java.util.List;
 
-	private ReactInstanceManager mReactInstanceManager;
-	private ReactRootView mReactRootView;
+public class MainActivity extends SalesforceReactActivity {
 
+	/**
+	 * Returns the name of the main component registered from JavaScript.
+	 * This is used to schedule rendering of the component.
+	 */
 	@Override
-	public void onResume(RestClient restClient) {
-        if (mReactInstanceManager == null) {
-            mReactInstanceManager = ReactInstanceManager.builder()
-				.setApplication(getApplication())
-				.setBundleAssetName("index.android.bundle")
-				.setJSMainModuleName("index.android")
-				.addPackage(new MainReactPackage())
-				.addPackage(SalesforceReactSDKManager.getInstance().getReactPackage())
-                .addPackage(new ImagePickerPackage())
-				.setUseDeveloperSupport(BuildConfig.DEBUG)
-				.setInitialLifecycleState(LifecycleState.RESUMED)
-				.build();
-
-            mReactRootView.startReactApplication(mReactInstanceManager, "MyUserPicReactNative", null);
-        }
+	protected String getMainComponentName() {
+		return "MyUserPicReactNative";
 	}
 
+	/**
+	 * Returns whether dev mode should be enabled.
+	 * This enables e.g. the dev menu.
+	 */
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		mReactRootView = new ReactRootView(this);
-		setContentView(mReactRootView);
+	protected boolean getUseDeveloperSupport() {
+		return BuildConfig.DEBUG;
 	}
 
+	/**
+	 * A list of packages used by the app. If the app uses additional views
+	 * or modules besides the default ones, add more packages here.
+	 */
 	@Override
-	public boolean onKeyUp(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-			mReactInstanceManager.showDevOptionsDialog();
-			return true;
-		}
-		return super.onKeyUp(keyCode, event);
-	}
-
-	@Override
-	public void invokeDefaultOnBackPressed() {
-		super.onBackPressed();
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		if (mReactInstanceManager != null) {
-			mReactInstanceManager.onPause();
-		}
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-
-		if (mReactInstanceManager != null) {
-			mReactInstanceManager.onResume(this, this);
-		}
+	protected List<ReactPackage> getPackages() {
+		return Arrays.<ReactPackage>asList(
+                new MainReactPackage(),
+                SalesforceReactSDKManager.getInstance().getReactPackage(),
+                new ImagePickerPackage()
+        );
 	}
 }
